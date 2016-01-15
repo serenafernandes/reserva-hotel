819f7352fc72a19ab57547e59340a9240b46bd13
Serena Fernandes <serena@MacBook-Air-de-Serena.local>
2016-01-15 02:26:33 -0200

    Adicionando HashCode e Equals para Tarifa

--- src/main/java/reserva/Tarifa.java
+++ src/main/java/reserva/Tarifa.java
@@ -4,6 +4,12 @@ public class Tarifa {
 	Hotel hotel;
 	Double preco;
 
+	public Tarifa(Hotel hotel, Double preco) {
+		super();
+		this.hotel = hotel;
+		this.preco = preco;
+	}
+
 	public Hotel getHotel() {
 		return hotel;
 	}
@@ -18,6 +24,37 @@ public class Tarifa {
 
 	public void setPreco(Double preco) {
 		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((hotel == null) ? 0 : hotel.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarifa other = (Tarifa) obj;
+		if (hotel == null) {
+			if (other.hotel != null)
+				return false;
+		} else if (!hotel.equals(other.hotel))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		return true;
 	}
 
 }
