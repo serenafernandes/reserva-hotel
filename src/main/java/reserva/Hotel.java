65760a47ffe658ce5189947a90a45a05b31bb235
Serena Fernandes <serena@MacBook-Air-de-Serena.local>
2016-01-15 02:26:15 -0200

    Adicionando HashCode e Equals para Hotel

--- src/main/java/reserva/Hotel.java
+++ src/main/java/reserva/Hotel.java
@@ -67,4 +67,49 @@ public class Hotel {
 		this.fimSemanaVip = fimSemanaVip;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + classificacao;
+		long temp;
+		temp = Double.doubleToLongBits(diaUtilRegular);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(diaUtilVip);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(fimSemanaRegular);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(fimSemanaVip);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hotel other = (Hotel) obj;
+		if (classificacao != other.classificacao)
+			return false;
+		if (Double.doubleToLongBits(diaUtilRegular) != Double.doubleToLongBits(other.diaUtilRegular))
+			return false;
+		if (Double.doubleToLongBits(diaUtilVip) != Double.doubleToLongBits(other.diaUtilVip))
+			return false;
+		if (Double.doubleToLongBits(fimSemanaRegular) != Double.doubleToLongBits(other.fimSemanaRegular))
+			return false;
+		if (Double.doubleToLongBits(fimSemanaVip) != Double.doubleToLongBits(other.fimSemanaVip))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
 }
