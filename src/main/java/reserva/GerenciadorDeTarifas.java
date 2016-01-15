851f80cf2bcfcd4dde1da5085532e84f6c706d9e
Serena Fernandes <serena@MacBook-Air-de-Serena.local>
2016-01-15 01:30:58 -0200

    Adicionando classe e teste para Tarifa

--- src/main/java/reserva/GerenciadorDeTarifas.java
+++ src/main/java/reserva/GerenciadorDeTarifas.java
@@ -6,9 +6,59 @@ import java.util.List;
 
 public class GerenciadorDeTarifas {
 
-	public Tarifa getMelhorTarifa(String tipoCliente, List<Date> periodo, ArrayList<Hotel> hoteis) {
+	public Tarifa getMelhorTarifa(TipoCliente tipoCliente, List<Date> periodo, List<Hotel> hoteis) {
 		// TODO Auto-generated method stub
+		return new Tarifa();
+	}
+
+	private String getHotelBaratoNome(String tipoCliente, Date initialDate, Date finalDate,
+			ArrayList<Hotel> hoteis) {
+
+		GerenciadorDeDatas gerenciadorDeDatas = new GerenciadorDeDatas();
+		
+		
+		ArrayList<Date> datas = (ArrayList<Date>) gerenciadorDeDatas.getPeriodo(initialDate, finalDate);
+		ArrayList<Integer> precos = new ArrayList<Integer>();
+		int menorPreco = 99999;
+		String hotelBarato = "";
+
+		for (Hotel hotel : hoteis) {
+			int sum = 0;
+			for (Date date : datas) {
+				if (tipoCliente.equals("regular")) {
+					if (gerenciadorDeDatas.isFimDeSemana(date)) {
+						sum += hotel.getFimSemanaRegular();
+					} else {
+						sum += hotel.getDiaUtilRegular();
+					}
+				} else if (tipoCliente.equals("rewards")) {
+					if (gerenciadorDeDatas.isFimDeSemana(date)) {
+						sum += hotel.getFimSemanaVip();
+					} else {
+						sum += hotel.getDiaUtilVip();
+					}
+				}
+			}
+			if (sum < menorPreco) {
+				menorPreco = sum;
+				hotelBarato = hotel.getNome();
+			} else if (sum == menorPreco) {
+				if (hotel.getClassificacao() > getHotelByName(hotelBarato, hoteis).getClassificacao()) {
+					hotelBarato = hotel.getNome();
+				}
+			}
+		}
+		return hotelBarato;
+	}
+
+	private Hotel getHotelByName(String name, ArrayList<Hotel> hoteis) {
+		for (Hotel hotel : hoteis) {
+			if (hotel.getNome().equals(name)) {
+				return hotel;
+			}
+		}
 		return null;
 	}
 
+	
 }
--- src/test/java/reserva/GerenciadorDeTarifasTest.java
+++ src/test/java/reserva/GerenciadorDeTarifasTest.java
@@ -1,20 +1,41 @@
 package reserva;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class GerenciadorDeTarifasTest {
 
+	GerenciadorDeTarifas gerenciadorDeTarifas;
+	List<Hotel> mockHoteis;
+	List<Date> mockPeriodo;
+
 	@Before
-	public void setup() {
-		GerenciadorDeTarifas gerenciadorDeTarifas = new GerenciadorDeTarifas();
+	public void setup() throws ParseException {
+		gerenciadorDeTarifas = new GerenciadorDeTarifas();
+		mockHoteis = new ArrayList<Hotel>();
+		mockHoteis.add(new Hotel("The Carlyle", 3, 110, 80, 90, 80));
+		mockHoteis.add(new Hotel("The Plaza", 4, 160.00, 110.00, 60.00, 50.00));
+		mockHoteis.add(new Hotel("Royal Hotel", 5, 220.00, 100.00, 150.00, 40.00));
+
+		mockPeriodo = new ArrayList<Date>();
+		
+		mockPeriodo.add(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2016"));
+		mockPeriodo.add(new SimpleDateFormat("dd/MM/yyyy").parse("02/01/2016"));
+		mockPeriodo.add(new SimpleDateFormat("dd/MM/yyyy").parse("03/01/2016"));
 	}
 
 	@Test
 	public void deveRetornarUmaTarifa() {
-		fail("Not yet implemented");
+		assertThat(gerenciadorDeTarifas.getMelhorTarifa(TipoCliente.Vip, mockPeriodo, mockHoteis), instanceOf(Tarifa.class));
 	}
 
 }
